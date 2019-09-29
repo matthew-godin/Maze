0e7c24ecf96500749947b765fe3548f5b199ec62
Matthew Godin <matthew.godin.software.engineer@gmail.com>
2019-09-29 02:24:05 -0400

    floating camera

--- webgl.js
+++ webgl.js
@@ -373,7 +373,7 @@ function drawScene(gl, programInfo, buffers, texture, deltaTime) {
   const fieldOfView = 45 * Math.PI / 180;   // in radians
   const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
   const zNear = 0.1;
-  const zFar = 100.0;
+  const zFar = 200.0;
   const projectionMatrix = mat4.create();
 
   // note: glmatrix.js always has the first argument
