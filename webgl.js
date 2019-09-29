838dc9b6fbb4edf4792154ea2e6a6508f947f8de
Matthew Godin <matthew.godin.software.engineer@gmail.com>
2019-09-28 22:23:24 -0400

    corners

--- webgl.js
+++ webgl.js
@@ -491,7 +491,7 @@ function drawScene(gl, programInfo, buffers, texture, deltaTime) {
     const offset = 0;
     gl.drawElements(gl.TRIANGLES, vertexCount, type, offset);
   }
-  var XZReal = 0.6;
+  var XZReal = 0.0;
   var XOffset = 1.0;
   var ZOffset = 1.0;
   var XReal = XTranslation + XOffset - XZReal;
