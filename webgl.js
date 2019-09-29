5ba1912b53f1881dbe423221d425c2be0d1cecfe
Matthew Godin <matthew.godin.software.engineer@gmail.com>
2019-09-29 00:53:08 -0400

    corners

--- webgl.js
+++ webgl.js
@@ -512,6 +512,10 @@ function drawScene(gl, programInfo, buffers, texture, deltaTime) {
     if (canvasMaze.getContext('2d').getImageData
       (XReal / 2, (ZReal + yCoord * 2.0 * deltaTime) / 2, 1, 1).data[0] == 255
       && canvasMaze.getContext('2d').getImageData
+        (XReal / 2, (ZReal2 + yCoord * 2.0 * deltaTime) / 2, 1, 1).data[0] == 255
+      && canvasMaze.getContext('2d').getImageData
+        (XReal2 / 2, (ZReal + yCoord * 2.0 * deltaTime) / 2, 1, 1).data[0] == 255
+      && canvasMaze.getContext('2d').getImageData
         (XReal2 / 2, (ZReal2 + yCoord * 2.0 * deltaTime) / 2, 1, 1).data[0] == 255) {
       ZTranslation += yCoord * 2.0 * deltaTime;
     }
