b47a203cd90cc218c58c52a8106be1307ea6fedf
Matthew Godin <matthew.godin.software.engineer@gmail.com>
2019-10-21 14:53:16 -0400

    commit

--- webgl.js
+++ webgl.js
@@ -411,7 +411,7 @@ function initBuffers(gl) {
   const textureCoordBuffer = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, textureCoordBuffer);
 
-  var t1 = 1.0 / 3.0, t2 = 2.0 / 3.0;
+  var t1 = 0.333333, t2 = 0.666666;
 
   var textureCoordinates = [];
   {
