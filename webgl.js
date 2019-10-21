701ffae0f2c77914600d25a3c8c84a24c1383fe0
Matthew Godin <matthew.godin.software.engineer@gmail.com>
2019-10-21 13:32:04 -0400

    commit

--- webgl.js
+++ webgl.js
@@ -221,7 +221,7 @@ function initBuffers(gl) {
           -halfSize + cubeOffsetX, oldHalfSize + cubeOffsetY, halfSize + cubeOffsetZ,
           halfSize + cubeOffsetX, oldHalfSize + cubeOffsetY, halfSize + cubeOffsetZ
         ]);
-    /*cubeOffsetX = 4.0 + bigOffset;
+    cubeOffsetX = 4.0 + bigOffset;
     cubeOffsetZ = 4.0 * mazeHeight + bigOffset;
     positions = positions.concat([
           -halfSize + cubeOffsetX, oldHalfSize + cubeOffsetY, -halfSize + cubeOffsetZ,
@@ -229,7 +229,7 @@ function initBuffers(gl) {
           -halfSize + cubeOffsetX, oldHalfSize + cubeOffsetY, halfSize + cubeOffsetZ,
           halfSize + cubeOffsetX, oldHalfSize + cubeOffsetY, halfSize + cubeOffsetZ
         ]);
-    cubeOffsetX = 2.0 + bigOffset;
+    /*cubeOffsetX = 2.0 + bigOffset;
     cubeOffsetZ = 4.0 + bigOffset;
     positions = positions.concat([
           -halfSize + cubeOffsetX, oldHalfSize + cubeOffsetY, -halfSize + cubeOffsetZ,
@@ -753,7 +753,7 @@ function drawScene(gl, programInfo, buffers, texture, deltaTime) {
   gl.uniform1i(programInfo.uniformLocations.uSampler, 0);
 
   {
-    const vertexCount = 24;//36 * mazeWidth * mazeHeight; + 10000;//+ 18 * 8 * mazeWidth * mazeHeight;
+    const vertexCount = 30;//36 * mazeWidth * mazeHeight; + 10000;//+ 18 * 8 * mazeWidth * mazeHeight;
     const type = gl.UNSIGNED_SHORT;
     const offset = 0;
     gl.drawElements(gl.TRIANGLES, vertexCount, type, offset);
